ea263910c2b9a76d6d7c0c44dfb39f6d32b355fa
Hwang08 <pmhwang8@gmail.com>
2019-11-18 17:43:08 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// Need to make a function that gets rid of the preloader
+// 'load' -> listens on load which basically tells when the website loads run the function.
+// window.addEventListener('load', () => {
+//     const preload = document.querySelector('.preload');
+//     preload.classList.add('preload-finish');
+// });
